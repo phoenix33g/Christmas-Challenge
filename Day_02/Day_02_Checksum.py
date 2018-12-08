d517079d22f8fd14b3415c7135c70ad33ceccd2f
phoenix <phoenix33g@gmail.com>
2018-12-08 17:28:30 -0500

    Working checksum proccessor

--- Day_02/Day_02_Checksum.py
+++ Day_02/Day_02_Checksum.py
@@ -5,10 +5,27 @@ import pprint as p
 checksum = 0
 value_list = [0,0]
 
-#Create array from input.txt file
+#Create list from input.txt file
 folder_path = "D:/_SCRIPTING/_ScriptProjects/25DaysTillChristmas-GITHUB/Day_02/"
 text_file = open(folder_path + "input.txt", "r")
 box_ids = text_file.read().split('\n')
 
+#Iterate through list
+for box_id in box_ids:
+    test_list = [False,False]
+    #Iterate through each box_id
+    for char in box_id:
+        #Test if box_id has a char repeating 2 or 3 times
+        temp = box_id.count(char)
+        if temp == 2: test_list[0] = True
+        if temp == 3: test_list[1] = True
+    #Add value to correct item
+    if test_list[0]: value_list[0] += 1
+    if test_list[1]: value_list[1] += 1
+
+#Create checksum
+checksum = value_list[0] * value_list[1]
+
 #Print out value
-p.pprint(box_ids)
+p.pprint(value_list)
+p.pprint(checksum)
