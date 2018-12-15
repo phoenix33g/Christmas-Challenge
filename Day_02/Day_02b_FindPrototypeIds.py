a4e45c092787fd6ceaed921a347743927e0ce29d
phoenix <phoenix33g@gmail.com>
2018-12-15 16:45:16 -0500

    finished day 2b challenge

--- Day_02/Day_02b_FindPrototypeIds.py
+++ Day_02/Day_02b_FindPrototypeIds.py
@@ -2,8 +2,7 @@
 import pprint as p
 
 #Variables
-checksum = 0
-value_list = [0,0]
+ids_list = []
 
 #Create list from input.txt file
 folder_path = "D:/_SCRIPTING/_ScriptProjects/25DaysTillChristmas-GITHUB/Day_02/"
@@ -11,21 +10,22 @@ text_file = open(folder_path + "input.txt", "r")
 box_ids = text_file.read().split('\n')
 
 #Iterate through list
-for box_id in box_ids:
-    test_list = [False,False]
-    #Iterate through each box_id
-    for char in box_id:
-        #Test if box_id has a char repeating 2 or 3 times
-        temp = box_id.count(char)
-        if temp == 2: test_list[0] = True
-        if temp == 3: test_list[1] = True
-    #Add value to correct item
-    if test_list[0]: value_list[0] += 1
-    if test_list[1]: value_list[1] += 1
-
-#Create checksum
-checksum = value_list[0] * value_list[1]
+for i in range(len(box_ids)-1):
+    box_id = box_ids[i]
+    #Iterate through remaining list
+    for j in range(i+1, len(box_ids)):
+        test_id = box_ids[j]
+        test_value = 0
+        temp_id = ""
+        #Iterate through each box_id
+        for x in range(len(box_id)):
+            #Test difference between box_id and test_id
+            if box_id[x] != test_id[x]:
+                test_value += 1
+            else:
+                temp_id = temp_id + box_id[x]
+        #Find and append ids with specified characteristics to ids_list
+        if test_value == 1: ids_list.append(temp_id)
 
 #Print out value
-p.pprint(value_list)
-print("Checksum: " + str(checksum))
+p.pprint(ids_list)
